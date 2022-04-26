d5fae673f7a4c2a50766d106178feaeff412952a
Rob Fournier <78333324+robfjr@users.noreply.github.com>
2022-04-26 08:47:59 -0500

    Add files via upload

---
+++ HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.7;
+
+contract HelloWorld {
+
+    string public happyGreeting = "Hello World!";
+    string public sadFarewell = "Goodbye, cruel world!";
+
+}
